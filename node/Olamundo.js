eba78de2361e8aee0235aab0b7620b92b5c5e318
JoãoFL <joaofilip444@gmail.com>
2025-04-15 22:05:12 -0300

    Add files via upload
    Testando Node pela primeira vez

---
+++ node/Olamundo.js
@@ -0,0 +1,6 @@
+console.log('Ola, mundo!!!!!')
+let text = 'Meu primeiro programa em Javascript utilizando note.js!!!'
+let n = 2;
+for (i = 0; i < 11; i++){
+    console.log(`${n} X ${i} = ${i*n}`)
+} 
